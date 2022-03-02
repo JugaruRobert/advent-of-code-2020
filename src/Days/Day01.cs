f0c504ea8afcdc2928ee3ce861ae9fa92136de18
Jugaru Robert <robertgeorgejugaru@gmail.com>
2022-03-02 23:43:03 +0200

    Day 1

--- src/Days/Day01.cs
+++ src/Days/Day01.cs
@@ -9,12 +9,52 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            List<int> numbers = input.Integers().ToList();
+            HashSet<int> uniqueNumbers = new();
+            int result = 0;
+
+            for(int i = 0; i < numbers.Count; i++)
+            {
+                int inverse = Math.Abs(numbers[i] - 2020);
+                if (uniqueNumbers.Contains(inverse))
+                {
+                    result = inverse * numbers[i];
+                    break;
+                }
+
+                uniqueNumbers.Add(numbers[i]);
+            }
+
+            return result.ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            List<int> numbers = input.Integers().ToList();
+            numbers.Sort();
+            int product = 0;
+
+            for(int i = 0; i < numbers.Count - 2; i++)
+            {
+                if (i > 0 && numbers[i] == numbers[i - 1])
+                    continue;
+
+                int left = i + 1, right = numbers.Count - 1;
+                
+                while(left < right)
+                {
+                    int sum = numbers[i] + numbers[left] + numbers[right];
+
+                    if (sum == 2020)
+                        return (numbers[i] * numbers[left] * numbers[right]).ToString();
+                    else if(sum > 2020)
+                        right--;
+                    else
+                        left++;
+                }
+            }
+
+            return product.ToString();
         }
     }
 }
